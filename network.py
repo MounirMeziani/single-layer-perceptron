998ceed84162ca315f7f177b58e965f2c075eda2
bennyp <benpang@outlook.com>
2018-01-30 10:50:07 -0500

    changed to 2 layer

--- network.py
+++ network.py
@@ -2,14 +2,13 @@ import numpy as np
 
 class mlp(object):
 
-	def __init__(self, hidden_size, num_inputs, num_outputs, learning_rate):
-		self._hidden_size = hidden_size
+	def __init__(self, num_inputs, num_outputs, learning_rate):
 		self._num_inputs = num_inputs
 		self._num_outputs = num_outputs
 		self._c = learning_rate
 
-		self.hidden_layer = np.random.randint(3, size=(self._hidden_size, self._num_inputs + 1))
-		self.output_layer = np.random.randint(3, size=(self._num_outputs, self._hidden_size))
+		#self.hidden_layer = np.random.randint(3, size=(self._hidden_size, self._num_inputs + 1))
+		self.output_layer = np.random.randint(3, size=(self._num_outputs, self._num_inputs + 1))
 
 	def _activation(self, z):
 		return float(1 / (1 + np.exp(-z))) #sigmoid activation
@@ -19,13 +18,13 @@ class mlp(object):
 		:param x:<list> 1 x m+1 feature vector
 		"""
 		print(x)
-		hidden_output = np.dot(self.hidden_layer, x.T)
-		hidden_output = map(hidden_output, self._activation)
+		output = np.dot(self.output_layer, x.T)
+		output = map(output, self._activation)
 
-		final_output = np.dot(self.output_layer, hidden_output)
-		final_output = np.map(final_output, self._activation)
+		#final_output = np.dot(self.output_layer, hidden_output)
+		#final_output = np.map(final_output, self._activation)
 
-		return np.argmax(final_output), hidden_output
+		return np.argmax(output)
 
 	def train(self, train_x, labels, num_epochs=1):
 
@@ -35,13 +34,12 @@ class mlp(object):
 			results = []
 
 			for i, x in enumerate(train_x):
-				y, hidden_output = self.eval(x)
+				y = self.eval(x)
 				results.append(y)
 
 				#adjust weights in each layer
 				sign = np.sign(labels[i] - y)
-				self.hidden_layer += sign * self._c * x
-				self.output_layer += sign * self._c * hidden_output
+				self.output_layer += sign * self._c * x
 
 			accuracy = accuracy_score(results, labels)
 			print(f'Epoch {epoch} - Training Accuracy: {accuracy}\n')
