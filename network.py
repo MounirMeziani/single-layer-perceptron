48be4e7453d65408641cefcf8208040a4c99534c
bennyp <benpang@outlook.com>
2018-01-30 20:17:32 -0500

    added ipython notebook doc

--- network.py
+++ network.py
@@ -1,54 +1,58 @@
 import numpy as np 
+from sklearn.metrics import accuracy_score
 
-class mlp(object):
+class two_layer_perceptron(object):
 
-	def __init__(self, num_inputs, num_outputs, learning_rate):
+	def __init__(self, num_inputs, num_outputs, learning_rate, threshold):
 		self._num_inputs = num_inputs
 		self._num_outputs = num_outputs
 		self._c = learning_rate
+		self._threshold = threshold
 
-		#self.hidden_layer = np.random.randint(3, size=(self._hidden_size, self._num_inputs + 1))
-		self.output_layer = np.random.randint(3, size=(self._num_outputs, self._num_inputs + 1))
+		self.output_layer = 3 - 6*np.random.random(size=(self._num_outputs, self._num_inputs + 1))
 
 	def _activation(self, z):
-		return float(1 / (1 + np.exp(-z))) #sigmoid activation
+		#sigmoid activation
+		#return 1 / (1+np.exp(-z))
+		result = 1 if z >= self._threshold else 0
+		return result
 	
 	def eval(self, x):
 		"""
 		:param x:<list> 1 x m+1 feature vector
 		"""
-		print(x)
 		output = np.dot(self.output_layer, x.T)
-		output = map(output, self._activation)
+		output = list(map(self._activation, output))
 
-		#final_output = np.dot(self.output_layer, hidden_output)
-		#final_output = np.map(final_output, self._activation)
+		return np.argmax(output), output
 
-		return np.argmax(output)
+	def train(self, train_x_raw, labels, num_epochs=1):
 
-	def train(self, train_x, labels, num_epochs=1):
-
+		train_x = np.hstack([train_x_raw, np.ones((train_x_raw.shape[0], 1))])
 		accuracy_log = []
 
 		for epoch in range(num_epochs):
 			results = []
 
 			for i, x in enumerate(train_x):
-				y = self.eval(x)
+				y, output = self.eval(x)
 				results.append(y)
 
-				#adjust weights in each layer
-				sign = np.sign(labels[i] - y)
-				self.output_layer += sign * self._c * x
+				for node in range(self._num_outputs):
+					if(output[node] == 1 and node != labels[i]):
+						self.output_layer[node] -= self._c * x
+					elif(output[node] == 0 and node == labels[i]):
+						self.output_layer[node] += self._c * x
 
 			accuracy = accuracy_score(results, labels)
-			print(f'Epoch {epoch} - Training Accuracy: {accuracy}\n')
+			#print(f'Epoch {epoch} - Training Accuracy: {accuracy}\n')
 			accuracy_log.append(accuracy)
 
 		return accuracy_log
 
-	def test(self, test_x):
+	def test(self, test_x_raw):
 
-		test_y = map(test_x, self.eval)
+		test_x = np.hstack(test_x_raw, np.ones((test_x_raw.shape[0], 1)))
+		test_y = map(self.eval, test_x)
 
 		return test_y
